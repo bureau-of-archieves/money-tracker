65237f93956a525bdbb44c61c01a2268fa273e8d
zhy2002 <hz141@uowmail.edu.au>
2016-01-17 21:32:22 +1100

    When build the client side code will be copied to the resources/public folder in the war file.

---
+++ money-tracker-webapp/src/client/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')({lazy: true});
+var wiredep = require('wiredep').stream;
+
+var ROOT_DIR = './money-tracker-webapp/src/main/resources/public';
+
+gulp.task('html', function(){
+
+    log('Processing the main html file..');
+    gulp.src(ROOT_DIR + '/index.html')
+        .pipe($.plumber())
+        .pipe(wiredep({}))
+        .pipe($.useref({searchPath: './'}))
+        .pipe(gulp.dest(ROOT_DIR));
+});
+
+gulp.task('default', ['html']);
+
+///////////////////// HELPER FUNCTIONS //////////////////////////
+
+function log(msg) {
+    var colored = $.util.colors.blue(msg);
+    console.log(colored);
+}
+
--- readme.md
+++ readme.md
@@ -1,6 +1,16 @@
 
 
 ##Dependencies
+###Gulp dependencies
+<pre>
+npm install --save-dev gulp
+npm install --save-dev gulp-load-plugins
+$ npm install --save-dev gulp-plumber
+$ npm install --save-dev gulp-util
+$ npm install --save-dev gulp-useref
+npm install --save-dev wiredep
+</pre>
+
 
 - Spring Boot
 
